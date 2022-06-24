40f066b5e7aa0978a595388fa48e3bdee39076c9
Komal-Jayswal <Komal.Jayswal@maersk.com>
2022-06-24 16:54:37 +0530

    Update readme file

---
+++ src/main/java/net/learning/Basic/Spring/Security/controller/Controller.java
@@ -0,0 +1,14 @@
+package net.learning.Basic.Spring.Security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller
+{
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "<h1>Hello User, have a nice day.<h1>";
+    }
+}
